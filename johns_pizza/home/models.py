db8e31c7050aeefb8b2062d71727efd5f9722c58
Johnathan Ingle <jingle1000@gmail.com>
2019-01-27 09:20:33 -0500

    Format Layout to accept context variable
    context variable on server side view passes avaliable food data to html template

--- johns_pizza/home/models.py
+++ johns_pizza/home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    category = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    size = models.CharField(max_length=20)
+    toppings = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.category} {self.name} {self.size} {self.num_toppings} {self.price}"
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=80)
+    def __str__(self):
+        return f"{self.name}"
+
+
+
+
+
--- johns_pizza/home/views.py
+++ johns_pizza/home/views.py
@@ -2,50 +2,64 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-pizza = [
+foods = [
     {
-        "Type":"Regular",
-        "Toppings": 0,
-        "Small": 12.20,
-        "Large": 17.45
+        "Category":"Pizza",
+        "Name": "Classic",
+        "Toppings": "Cheese",
+        "Size": "Small",
+        "Price": 17.45
     },
     {
-        "Type":"Regular",
-        "Toppings": 1,
-        "Small": 13.20,
-        "Large": 19.45
+        "Category":"Pizza",
+        "Name": "Classic",
+        "Toppings": "Cheese",
+        "Size": "Medium",
+        "Price": 17.45
     },
     {
-        "Type":"Regular",
-        "Toppings": 2,
-        "Small": 12.20,
-        "Large": 17.45
+        "Category":"Pizza",
+        "Name": "Sicilian",
+        "Toppings": "Cheese",
+        "Size": "Large",
+        "Price": 17.45
     },
     {
-        "Type":"Sicilian",
-        "Toppings": 0,
-        "Small": 12.20,
-        "Large": 17.45
+        "Category":"Pasta",
+        "Name": "Baked Ziti w/Mozzarella",
+        "Toppings": "N/A",
+        "Size": "Regular",
+        "Price": 17.45
     },
     {
-        "Type":"Sicilian",
-        "Toppings": 1,
-        "Small": 12.20,
-        "Large": 17.45
-    },
-    {
-        "Type":"Sicilian",
-        "Toppings": 3,
-        "Small": 12.20,
-        "Large": 17.45
+        "Category":"Pasta",
+        "Name": "Baked Ziti w/Meatballs",
+        "Toppings": "",
+        "Size": "Regular",
+        "Price": 17.45
     },
 ]
 
 def index(request):
+    food_data = {}
+    for food in foods:
+        category = food["Category"]
+        food_attrs = {"name":[food['Name']], "toppings":[food['Toppings']], "size":[food['Size']], "price":[food['Price']]}
+        try:
+            if food['Name'] not in list(food_data[category][0]["name"]):
+                food_data[category][0]["name"].append(food['Name'])
+            if food['Toppings'] not in list(food_data[category][0]["toppings"]):
+                food_data[category][0]["toppings"].append(food['Toppings'])
+            if food['Size'] not in list(food_data[category][0]["size"]):
+                food_data[category][0]["size"].append(food['Size'])
+            food_data[category][0]["price"].append(food['Price'])
+        except KeyError:
+            food_data[category] = []
+            food_data[category].append(food_attrs)
     context = {
-        'pizza':pizza
+        "foods": food_data
     }
-    return render(request, 'home/index.html', context)
+    return render(request, 'home/index.html', context=context)
 
 def about(request):
     return render(request, 'home/about.html')
