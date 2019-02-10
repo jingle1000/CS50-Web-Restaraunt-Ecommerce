c39d442bac05696f1a2d9a9a4d8bdb7e20b9f494
Johnathan Ingle <jingle1000@gmail.com>
2019-02-10 18:10:04 -0500

    Add Check Price Feature

--- johns_pizza/home/static/home/js/main.js
+++ johns_pizza/home/static/home/js/main.js
@@ -1,11 +1,38 @@
+
 document.addEventListener("DOMContentLoaded", function() {
     const addButtons = document.querySelectorAll('#add-item');
-    
     addButtons.forEach(button => {
+        foodData = [];
         button.addEventListener("click", function(e){
             const buttonID = e.target.getAttribute('data-number');
+            heading = document.querySelector(`[data-type='${buttonID}']`);
+            foodData.push(heading.innerText);
             const inputs = document.querySelectorAll(`[data-id='${buttonID}']`);
-            inputs.forEach(item => console.log(item.value));
+            inputs.forEach(item => foodData.push(item.value));
+            getPrice(foodData, buttonID);
         });
     })
 });
+
+function getPrice(foodData, buttonID) {
+    const request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                data = this.responseText;
+                data = JSON.parse(data);
+                if (data) {
+                    price = data.price;
+                    updatePrice(price, buttonID);
+                }
+            }
+        }
+        request.open('GET', `/${foodData[0]}/${foodData[1]}/${foodData[3]}/${foodData[2]}`, true);
+        request.send();
+}
+
+function updatePrice(price, buttonID) {
+    console.log(price + " " + buttonID);
+    const input = document.querySelector(`[data-amount='${buttonID}']`);
+    console.log(input);
+    input.value = price;
+}
